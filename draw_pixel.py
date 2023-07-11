86b595b81b2de8f6ca9f4944dd3390be22bc90ca
Johan Karlsson <44366788+ohank@users.noreply.github.com>
2023-07-11 18:40:35 +0200

    Changed draw pixel to draw multiple pixels

--- draw_pixel.py
+++ draw_pixel.py
@@ -5,9 +5,12 @@ class ImageCreator():
         self.draw_canvans = Image.new(mode='P', size=size, color=color)
 
         
-    def draw_pixel(self, color: str, position: tuple | list[tuple]) -> None:
-        self.draw_canvans.putpixel(position, ImageColor.getcolor(color, 'P'))
+    def draw_pixel(self, color: str, position: tuple[int, int] | list[tuple[int, int]]) -> None:
+        if isinstance(position, list[tuple]):
+            for pos in position:
+                self.draw_canvans.putpixel(pos, ImageColor.getcolor(color, 'P'))
+        else:
+            self.draw_canvans.putpixel(position, ImageColor.getcolor(color, 'P'))
 
     def save_image(self, file_name: str = 'simpelPixel.png') -> None:
         self.draw_canvans.save(file_name)
-
