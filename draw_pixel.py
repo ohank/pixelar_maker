5ea15b232334214f0d0a7ab3e91a4379df073a2b
Johan Karlsson <44366788+ohank@users.noreply.github.com>
2023-07-13 21:05:37 +0200

    added draw line

--- draw_pixel.py
+++ draw_pixel.py
@@ -1,11 +1,14 @@
 from PIL import Image, ImageColor
+from typing import Union
 
 class ImageCreator():
     def __init__(self, size: tuple = (25,25), color: str  = "white") -> None:
+        self._height = size[0]
+        self._width = size[1]
         self.draw_canvans = Image.new(mode='P', size=size, color=color)
 
         
-    def draw_pixel(self, color: str, position: tuple[int, int] | list[tuple[int, int]]) -> None:
+    def draw_pixel(self, color: str, position: Union[tuple, list[tuple]]) -> None:
         if isinstance(position, list[tuple]):
             for pos in position:
                 self.draw_canvans.putpixel(pos, ImageColor.getcolor(color, 'P'))
@@ -14,3 +17,7 @@ class ImageCreator():
 
     def save_image(self, file_name: str = 'simpelPixel.png') -> None:
         self.draw_canvans.save(file_name)
+    
+    def draw_horizontal_line(self, color: str) -> None:
+        self.draw_pixel(color=color, position=
+                        list(zip(range(self._width),([0] * self._height))))
